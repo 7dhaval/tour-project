e7e8c3677155ab7214aa80560647a7f0f8aa8271
dhavals <dhavalsinhs@zignuts.com>
2023-12-04 19:03:08 +0530

    added more package for helmet and routes

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const fs = require("fs");
 const morgan = require("morgan");
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+
 const app = express();
 
 const AppError = require("./utils/appError");
@@ -8,12 +11,29 @@ const globalErrorHandler = require('./controllers/errorController')
 const tourRouter = require("./routes/tourRoutes");
 const userRouter = require("./routes/userRoutes");
 
+//1)Global Middlewares
+//set securtiy http header
+app.use(helmet())
 //Middlewares
+//env
 console.log(process.env.NODE_ENV);
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
-app.use(express.json());
+
+//Limit requests from same Api
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too may requests from this IP please try again in an hour!'
+});
+
+app.use('/api',limiter);
+
+//Body parser,reading data from body into req.body
+app.use(express.json({limit: '10kb'}));
+
+//serving static files
 app.use(express.static(`${__dirname}/public`));
 
 // app.use((req, res, next) => {
@@ -21,6 +41,7 @@ app.use(express.static(`${__dirname}/public`));
 //   next();
 // });
 
+//Test middlewares
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   // console.log(req.headers);
@@ -30,6 +51,7 @@ app.use((req, res, next) => {
 app.use("/api/v1/tours", tourRouter);
 app.use("/api/v1/users", userRouter);
 
+//Unhandle routes
 app.all("*", (req, res, next) => {
   //   res.status(404).json({
   //     status: "fail",
