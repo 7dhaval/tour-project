0e2b785365e66e10374defa80b6ca3c0c24d94e9
dhavals <dhavalsinhs@zignuts.com>
2023-12-13 16:13:40 +0530

    prevent duplicate

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-const Tour = require('./../models/tourModel');
+const Tour = require("./../models/tourModel");
 
 const reviewSchema = new mongoose.Schema(
   {
@@ -33,70 +33,70 @@ const reviewSchema = new mongoose.Schema(
   }
 );
 
+reviewSchema.index({ tour: 1, user: 1 }, { unique: true });
 
-reviewSchema.pre(/^find/, function(next){
-    // this.populate({
-    //     path: 'tour',
-    //     select:'name'
-    // }).populate({
-    //     path: 'user',
-    //     select: 'name photo'
-    // })
+reviewSchema.pre(/^find/, function(next) {
+  // this.populate({
+  //     path: 'tour',
+  //     select:'name'
+  // }).populate({
+  //     path: 'user',
+  //     select: 'name photo'
+  // })
 
-    this.populate({
-        path: 'user',
-        select: 'name photo'
-    })
+  this.populate({
+    path: "user",
+    select: "name photo",
+  });
 
-    next();
+  next();
 });
 
-reviewSchema.statics.calcAverageRatings = async function(tourId){
+reviewSchema.statics.calcAverageRatings = async function(tourId) {
   const stats = await this.aggregate([
     {
-      $match: {tour: tourId}
+      $match: { tour: tourId },
     },
     {
-      $group:{
-        _id: '$tour',
-        nRatings: { $sum: 1},
-        avgRating: {$avg: '$rating'}
-      }
-    }
+      $group: {
+        _id: "$tour",
+        nRatings: { $sum: 1 },
+        avgRating: { $avg: "$rating" },
+      },
+    },
   ]);
   console.log(stats);
 
-
-  if(stats.length > 0 ) {
-  await Tour.findByIdAndUpdate(tourId, {
-    ratingsQuantity: stats[0].nRatings,
-    ratingsAverage: stats[0].avgRating
-  })} else{
+  if (stats.length > 0) {
+    await Tour.findByIdAndUpdate(tourId, {
+      ratingsQuantity: stats[0].nRatings,
+      ratingsAverage: stats[0].avgRating,
+    });
+  } else {
     await Tour.findByIdAndUpdate(tourId, {
       ratingsQuantity: 0,
-      ratingsAverage: 4.5
-    })
+      ratingsAverage: 4.5,
+    });
   }
 };
 
-reviewSchema.post('save', function(){
+reviewSchema.post("save", function() {
   //this points to current review
 
   this.constructor.calcAverageRatings(this.tour);
-})
-
+});
 
 //findByIdAndUpdate
 //findByIdAndDelete
 
-reviewSchema.pre(/^findOneAnd/, async function(next){
+reviewSchema.pre(/^findOneAnd/, async function(next) {
   this.r = await this.findOne();
   console.log(this.r);
   next();
 });
 
-reviewSchema.post(/^findOneAnd/, async function(){
-  await this.r.constructor.calcAverageRatings(this.r.tour)
+reviewSchema.post(/^findOneAnd/, async function() {
+  await this.r.constructor.calcAverageRatings(this.r.tour);
 });
 
 const Review = mongoose.model("Review", reviewSchema);
