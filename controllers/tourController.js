29e9cd57a68ef12b16ed4448eb5ee98aae23a0de
dhavals <dhavalsinhs@zignuts.com>
2023-12-14 18:17:49 +0530

    complete multi upload feat

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -3,6 +3,64 @@ const APIFeatures = require("./../utils/apiFeatures");
 const catchAsync = require("../utils/catchAsync");
 const AppError = require("../utils/appError");
 const factory = require("./../controllers/handlerFactory");
+const multer = require("multer");
+const sharp = require("sharp");
+
+const multerStorage = multer.memoryStorage();
+
+const multerFilter = (req, file, cb) => {
+  if (file.mimetype.startsWith("image")) {
+    cb(null, true);
+  } else {
+    cb(new AppError("Not an Image ! please upload only Images.", 400), false);
+  }
+};
+const upload = multer({
+  storage: multerStorage,
+  fileFilter: multerFilter,
+});
+
+exports.uploadTourImages = upload.fields([
+  { name: "imageCover", maxCount: 1 },
+  { name: "images", maxCount: 3 },
+]);
+
+// upload.single('image')
+// upload.array('images', 5)
+
+exports.resizeTourImages = async (req, res, next) => {
+  console.log(req.files);
+  
+  if(!req.files.imageCover || !req.files.images) return next();
+
+  //1) cover image
+  req.body.imageCover = `tour-${req.params.id}-${Date.now()}-cover.jpeg`;
+  await sharp(req.files.imageCover[0].buffer)
+    .resize(2000, 1333)
+    .toFormat("jpeg")
+    .jpeg({ quality: 90 })
+    .toFile(`public/img/tours/${req.body.imageCover}`);
+
+  // req.body.imageCover = imageCoverFileName;
+  //2) Images
+  req.body.images = [];
+  await Promise.all(req.files.images.map(async(file, i) => {
+    const filename = `tour-${req.params.id}-${Date.now()}-${i + 1}.jpeg`;
+
+    await sharp(file.buffer)
+    .resize(2000, 1333)
+    .toFormat("jpeg")
+    .jpeg({ quality: 90 })
+    .toFile(`public/img/tours/${filename}`);
+
+
+  req.body.images.push(filename);  
+
+  })
+  );
+  // console.log();
+  next();
+};
 
 exports.aliasTopTours = (req, res, next) => {
   req.query.limit = "5";
@@ -246,18 +304,16 @@ exports.getToursWithIn = catchAsync(async (req, res, next) => {
   });
 });
 
-
-
 exports.getDistances = catchAsync(async (req, res, next) => {
   const { latlng, unit } = req.params;
-  const [lat, lng] = latlng.split(',');
+  const [lat, lng] = latlng.split(",");
 
-  const multiplier = unit === 'mi' ? 0.000621371 : 0.001;
+  const multiplier = unit === "mi" ? 0.000621371 : 0.001;
 
   if (!lat || !lng) {
     next(
       new AppError(
-        'Please provide latitutr and longitude in the format lat,lng.',
+        "Please provide latitutr and longitude in the format lat,lng.",
         400
       )
     );
@@ -267,25 +323,25 @@ exports.getDistances = catchAsync(async (req, res, next) => {
     {
       $geoNear: {
         near: {
-          type: 'Point',
-          coordinates: [lng * 1, lat * 1]
+          type: "Point",
+          coordinates: [lng * 1, lat * 1],
         },
-        distanceField: 'distance',
-        distanceMultiplier: multiplier
-      }
+        distanceField: "distance",
+        distanceMultiplier: multiplier,
+      },
     },
     {
       $project: {
         distance: 1,
-        name: 1
-      }
-    }
+        name: 1,
+      },
+    },
   ]);
 
   res.status(200).json({
-    status: 'success',
+    status: "success",
     data: {
-      data: distances
-    }
+      data: distances,
+    },
   });
 });
